33bdb2a4097f8121543bf458f06e0cb48ce7f247
Skorpion96 <39702358+Skorpion96@users.noreply.github.com>
2024-03-30 12:39:38 +0100

    Adding the main script

---
+++ dump_firmware.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+# Create dump directory
+mkdir /sdcard/dump
+
+# List all partition names in /dev/block/by-name
+for partition in $(ls /dev/block/by-name); do
+    # Skip userdata partition
+    if [ "$partition" == "userdata" ]; then
+        echo "Skipping userdata partition..."
+        continue
+    fi
+    
+    # Skip partitions with names like sda, sdb, sdc, sdd, sde
+    if echo "$partition" | grep -qE '^(sda|sdb|sdc|sdd|sde|sdf)$'; then
+        echo "Skipping $partition..."
+        continue
+    fi
+
+    echo "Dumping $partition..."
+    
+    # Use dd to dump each partition
+    dd if=/dev/block/by-name/$partition of=/sdcard/dump/$partition.img
+done
